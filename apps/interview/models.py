9db898608b289ee8c82bca9421ef8fda968bb3c6
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-10-06 18:15:28 +0500

    feat:user model changed

--- apps/interview/models.py
+++ apps/interview/models.py
@@ -21,15 +21,15 @@ class InterviewTag(BaseModel):
 class Interview(BaseModel, NewsBase):
     tag = models.ManyToManyField(
         InterviewTag,
-        related_name="interview_tag",
+        related_name='interview_tag',
     )
     subtitle = models.CharField(max_length=255)
-    video_url = models.URLField(verbose_name=_("Video Url"))
+    video_url = models.URLField(verbose_name=_('Video Url'))
     is_main = models.BooleanField(default=False)
 
     def __str__(self):
         return self.subtitle
 
     class Meta:
-        verbose_name = "Interview"
-        verbose_name_plural = "Interviews"
+        verbose_name = 'Interview'
+        verbose_name_plural = 'Interviews'
--- apps/users/models.py
+++ apps/users/models.py
@@ -18,9 +18,11 @@ from apps.common.models import BaseModel
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, password, **extra_fields):
+
+    def _create_user(self, phone_number, password, **extra_fields):
         if not phone_number:
             raise ValueError("The given phone number must be set")
+
         user = self.model(phone_number=phone_number, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
