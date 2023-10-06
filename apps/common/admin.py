6f3dc84739b5d7d625908d3cfda52b9cf6ddecbc
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-10-06 17:23:22 +0500

    feat: all models of news created

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from apps.common.models import Advertising, SocialMedia
 
+
 @admin.register(Advertising)
 class AdvertisingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'file', 'type')  # Customize the list display fields
-    list_filter = ('type',)  # Add filtering options based on 'type'
-    search_fields = ('type',)  # Enable search by 'type'
+    list_display = ("id", "file", "type")  # Customize the list display fields
+    list_filter = ("type",)  # Add filtering options based on 'type'
+    search_fields = ("type",)  # Enable search by 'type'
+
 
 @admin.register(SocialMedia)
 class SocialMediaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url', 'number', 'desc')  # Customize the list display fields
-    search_fields = ('url', 'desc')  # Enable search by 'url' and 'desc'
+    list_display = ("id", "url", "number", "desc")  # Customize the list display fields
+    search_fields = ("url", "desc")  # Enable search by 'url' and 'desc'
---
+++ apps/news/choices.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class NewsPositionChoices(models.TextChoices):
+    MAIN = "main", "Main"
+    PRIME = "prime", "Prime"
+    ORDINARY = "ordinary", "Ordinary"
+
+
+class NewsStatusChoices(models.TextChoices):
+    DRAFT = "draft", "Draft"
+    IN_MODERATION = "in moderation", "In moderation"
+    PUBLISHED = "published", "Published"
+
+
+class NewsTypeChoices(models.TextChoices):
+    SPECIAL = "special report", "Special Report"
+    NEWS = "news", "News"
+
+
