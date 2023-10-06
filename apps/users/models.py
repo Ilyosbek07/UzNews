084f46ad7b84eb395f45fd207ed59b087ca22a08
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-10-06 18:17:16 +0500

    feat:user model changed

--- apps/users/models.py
+++ apps/users/models.py
@@ -18,11 +18,18 @@ from apps.common.models import BaseModel
 
 
 class UserManager(BaseUserManager):
-
     def _create_user(self, phone_number, password, **extra_fields):
         if not phone_number:
             raise ValueError("The given phone number must be set")
 
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, password, **extra_fields):
+        if not phone_number:
+            raise ValueError("The given phone number must be set")
         user = self.model(phone_number=phone_number, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
